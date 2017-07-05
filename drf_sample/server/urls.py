20cd619a42650575f3d250f5ec4fcdace1dac8a8
shiddiqeuy <shiddiqitt@gmail.com>
2017-07-05 21:36:53 +0700

    edit context disapcther

--- drf_sample/server/urls.py
+++ drf_sample/server/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from retail.views import ChainViewSet, StoreViewSet, EmployeeViewSet
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+router = DefaultRouter()
+router.register(prefix='chains', viewset=ChainViewSet)
+router.register(prefix='stores', viewset=StoreViewSet)
+router.register(prefix='employees', viewset=EmployeeViewSet)
+
+urlpatterns = routers.urls
+
